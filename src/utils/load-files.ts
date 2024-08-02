d6fadb1f35b2bf34e3cc96cfd294555d20bd876f
lavagri <vlad.lazurenko@gmail.com>
2024-08-02 12:25:47 +0300

    fix(ms): lua script, docs missing in build + minor mock blacklist issues

--- src/utils/load-files.ts
+++ src/utils/load-files.ts
@@ -1,22 +1,15 @@
-import glob from 'glob'
+import { globSync } from 'glob'
 import path from 'path'
 
 export function loadMockingFiles(pattern: string) {
-  glob(pattern, (globErr, files) => {
-    if (globErr) {
-      // eslint-disable-next-line no-console
-      console.error('Error while scanning for mocking files:', globErr)
-      return
-    }
+  const files = globSync(pattern)
 
-    // Require each file
-    files.forEach((file) => {
-      try {
-        require(path.resolve(file))
-      } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(`Error while loading ${file}:`, err)
-      }
-    })
+  files.forEach((file) => {
+    try {
+      require(path.resolve(file))
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(`Error while loading ${file}:`, err)
+    }
   })
 }
